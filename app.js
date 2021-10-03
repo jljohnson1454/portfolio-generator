ce4b6a205c694c7c946f9fc5c671a4de2bbdbd53
Ghidorah <jljohnson1454@gmail.com>
2021-10-03 08:48:23 -0500

    added question

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name,github] = profileDataArgs;
-
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+// const pageHTML = gneratePage(name, github);
 
 
+// // const printProfileData = profileDataArr => {
+// //   // This...
+// //   for (let i = 0; i < profileDataArr.length; i += 1) {
+// //     console.log(profileDataArr[i]);
+// //   }
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw new Error(err);
+// //   console.log('================');
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// //   // Is the same as this...
+// //   profileDataArr.forEach(profileItem => console.log(profileItem));
+// // };
+
+// // printProfileData(profileDataArgs);
+
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if(err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type:'input',
+            name:'name',
+            message: 'What is your name?'
+        }
+    ]).then(answers => console.log(answers));
